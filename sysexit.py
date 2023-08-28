cc1c105167ab176b4d73a65497f35698110a4b36
julianmpineda <82466536+julianmpineda@users.noreply.github.com>
2023-08-28 16:00:08 -0400

    Create sysexit.py
    Section 3, Video 8:  Python's Built-in Functions
    
    Just some playing around with randint and testing sys.exit()

---
+++ sysexit.py
@@ -0,0 +1,13 @@
+import sys
+import random
+
+print(random.randint(0,10))
+print(random.randint(10,10))
+print(random.randint(0,100))
+print(random.randint(0,1000))
+
+print('Hello')
+sys.exit()
+print('Goodbye')
+
+# testing non comments (<- do not work even with sys.exit() must be commented)
